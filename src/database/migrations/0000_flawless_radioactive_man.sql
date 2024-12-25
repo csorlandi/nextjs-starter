db8f003da8e77b7949d63b56f2328c31081f67e0
Claudio Orlandi <claudiosorlandi@gmail.com>
2024-12-25 09:50:12 +0100

    fix: remove user email verified not null rule

--- src/database/migrations/0000_flawless_radioactive_man.sql
+++ src/database/migrations/0000_flawless_radioactive_man.sql
@@ -23,7 +23,7 @@ CREATE TABLE "user" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" varchar(255),
 	"email" varchar(320) NOT NULL,
-	"emailVerified" timestamp NOT NULL,
+	"emailVerified" timestamp,
 	"image" varchar(2048) NOT NULL,
 	CONSTRAINT "user_email_unique" UNIQUE("email")
 );
