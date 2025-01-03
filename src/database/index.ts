6b1cc67c7d2d273ce926ffc2330514fa64a76b19
Claudio Orlandi <claudiosorlandi@gmail.com>
2025-01-03 13:50:07 +0100

    feat: enable database query autocomplete based on schema

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,11 @@
 import { env } from '@/lib/env/server';
 
+import * as schema from './schema';
+
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-const db = drizzle(env.DATABASE_URL!);
+const db = drizzle(env.DATABASE_URL!, {
+  schema,
+});
 
 export default db;
